a0cab4f74c61c120ed61b1fda488d29eeb92dab6
AgentosUA <agentowork@gmail.com>
2019-02-10 20:16:07 +0200

    first part done

--- FLX_homework_7/homework/src/js/task1.js
+++ FLX_homework_7/homework/src/js/task1.js
@@ -1,37 +1,49 @@
 var
     askLogin = prompt('Enter your login','User'),
-    checkLenght = askLogin.length < 4;
     userPass = 'UserPass',
     adminPass = 'RootPass',
     currentTime = new Date().getHours();
 
-    switch(askLogin) {
-        case '':
-            alert('Canceled');
-            break;
-        case null:
-            alert('Canceled');
-            break;
-        case 'User':
-            break;
-        case 'Admin':
-            break;
-        default:
-            alert('I don’t know you');
-            break;
+    if(!askLogin) {
+        alert('Canceled');
     }
 
+    if(askLogin.length < 4) {
+        alert('i don\'t know any users having name length less than 4 symbols');
+    } else {
+        switch(askLogin) {
+            case '':
+                alert('Canceled');
+                break;
+            case 'User':
+                break;
+            case 'Admin':
+                break;
+            default:
+                alert('I don\'t know you');
+                break;
+        }
+    }
+
+    // Time checker
+    if(currentTime >= 20) {
+        currentTime = 'evening';
+    } else {
+        currentTime = 'day';
+    }
+
+    // Pass checker
     if (askLogin === 'User') {
         let askPass = prompt('Enter your pass');
         if(askPass === userPass) {
-            
+            alert('Good ' + currentTime + ', User!');
         } else {
             alert('Wrong password!');
         }
     } else if (askLogin === 'Admin') {
         let askPass = prompt('Enter your pass');
-        if(askPass === rootPass) {
-
+        if(askPass === adminPass) {
+            alert('Good ' + currentTime + ', Admin!');
         } else {
             alert('Wrong password!');
         }
