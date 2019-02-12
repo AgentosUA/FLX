095e7fe75f7747ab6d63bc807f1bc6d15e06a714
AgentosUA <agentoswork@gmail.com>
2019-02-12 19:54:48 +0200

    fixed float number situation

--- FLX_homework_8/homework/js/reverseNumber.js
+++ FLX_homework_8/homework/js/reverseNumber.js
@@ -1,9 +1,9 @@
 function reverseNumber(number) {
-    number = number.toString(10)
-    if(number.charAt(0) === '-') {
-      return number.charAt(0) + number.slice(1).split('').reverse().join('');
-    } else {
-      return number.split('').reverse().join('').slice(1);
-    }      
-  }
+	number = number.toString(10)
+	if (number.charAt(0) === '-') {
+		return parseInt(number.charAt(0) + number.slice(1).split('').reverse().join(''));
+	} else {
+		return parseInt(number.split('').reverse().join(''));
+	}
+}
 reverseNumber(-281);
