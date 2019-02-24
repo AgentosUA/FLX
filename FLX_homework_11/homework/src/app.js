087eef1287417b53b254beab8960ab39eb7eb8b1
AgentosUA <agentoswork@gmail.com>
2019-02-24 12:05:40 +0200

    first commit

---
+++ FLX_homework_11/homework/src/app.js
@@ -0,0 +1,9 @@
+let rootNode = document.getElementById('root');
+
+let addNew = () => {
+    let text = document.getElementsByClassName('list-input')[0].value;
+        
+}
+
+let button = document.getElementById('button');
+button.addEventListener('click', addNew, false);
